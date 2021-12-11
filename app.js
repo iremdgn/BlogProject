a388f2b5a17f177b9ed170990c846adfd9f1a75b
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-11 12:59:53 +0300

    update

--- app.js
+++ app.js
@@ -12,16 +12,46 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
 
-app.get('/', function(req, res) {
-  res.render('home', {startingContent: homeStartingContent});
+app.get('/', function (req, res) {
+
+  res.render('home', { startingContent: homeStartingContent });
+  console.log(posts);
 
 });
 
 
+app.get('/about', function (req, res) {
+
+  res.render('about', { aboutContent: aboutContent });
+
+});
+
+app.get('/contact', function (req, res) {
+
+  res.render('contact', { contactContent: contactContent });
+
+});
+
+app.get('/compose', function (req, res) {
+  res.render('compose');
+});
+
+app.post('/compose', function (req, res) {
+
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+  res.redirect("/");
+
+})
 
 
 
@@ -33,6 +63,7 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(3000, function() {
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
